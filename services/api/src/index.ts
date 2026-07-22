8043c9a948f541f987f4367f7089e5d7647db1ca
berkeroglu2234 <berkeroglu2234@gmail.com>
2026-07-22 15:11:41 +0300

    Update index.ts

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -74,10 +74,12 @@ app.get('/pricing', async (_, res) => {
 
 app.get('/requests', auth, async (req: AuthedRequest, res) => {
   try {
-    let sql = `SELECT  vr.*, vr.distance_km AS "distanceKm", vr.quoted_price AS "quotedPrice", d.full_name AS driver_name, d.phone AS driver_phone, COALESCE(AVG(rt.score),0)::float AS driver_rating
+    let sql = `SELECT  vr.*, vr.distance_km AS "distanceKm", vr.quoted_price AS "quotedPrice", c.phone AS "customerPhone", d.full_name AS driver_name, d.phone AS driver_phone, COALESCE(AVG(rt.score),0)::float AS driver_rating
       FROM valet_requests vr
       LEFT JOIN users d ON d.id=vr.driver_id
+      LEFT JOIN users c ON c.id=vr.customer_id
       LEFT JOIN ratings rt ON rt.driver_id=vr.driver_id`;
+    
     const params: any[] = [];
     if (req.user?.role === 'CUSTOMER') { sql += ' WHERE vr.customer_id=$1'; params.push(req.user.id); }
     if (req.user?.role === 'DRIVER') { sql += " WHERE vr.driver_id=$1 OR vr.status='SEARCHING'"; params.push(req.user.id); }
